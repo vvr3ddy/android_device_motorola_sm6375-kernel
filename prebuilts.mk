44c4d518beee37651e5af04d7f43dae41949a0a7
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-07-23 11:27:21 +0100

    fogos: Setup for Prebuilt compilation
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

---
+++ prebuilts.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_KERNEL_PATH := device/motorola/sm6375-kernel
+
+DEVICE_KERNEL_PATH := $(COMMON_KERNEL_PATH)/$(TARGET_DEVICE)
+
+# Flag required to use prebuilt kernel (LineageOS)
+TARGET_FORCE_PREBUILT_KERNEL := true
+
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_KERNEL_PATH)/dtbo.img
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_KERNEL_PATH)/dtb
+TARGET_PREBUILT_KERNEL := $(DEVICE_KERNEL_PATH)/kernel
+
+# Copy out modules
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_KERNEL_PATH)/modules/vendor/,$(TARGET_COPY_OUT_VENDOR)/lib/modules)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_KERNEL_PATH)/modules/vendor_ramdisk/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules)
